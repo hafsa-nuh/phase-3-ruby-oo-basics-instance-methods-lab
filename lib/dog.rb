9047757c49cac433f43dad24e726971cff5daac9
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-17 20:39:14 +0300

    done

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    # Instance Method
+    def bark 
+        puts "Woof!"
+    end
+    def sit
+        puts "The Dog is sitting" 
+    end
+end
+ 
+rose = Dog.new
+rose.bark
+rose.sit
+snoopy = Dog.new
+snoopy.bark
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+person1 = Person.new
+person1.talk
+person2 = Person.new
+person2.walk
